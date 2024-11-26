031f3ec2d08dca8f1904f7f371771a0f3876748a
adrianm <adrian.muscat@systech.net>
2024-11-26 16:49:26 +0100

    first commit

---
+++ Get-HotFixStatus.ps1
@@ -0,0 +1,25 @@
+Function Get-HotFixStatus{
+    param(
+        $Id,
+        $Computer
+    )
+ 
+    $Found = $false
+    try{
+        $HotFix = @{
+            Id           = $Id
+            ComputerName = $Computer
+            ErrorAction  = 'Stop'
+        }
+        Get-HotFix @HotFix | Out-Null
+        $Found = $true
+    }
+    catch{
+        $NotFound = 'GetHotFixNoEntriesFound,' +
+            'Microsoft.PowerShell.Commands.GetHotFixCommand'
+        if($_.FullyQualifiedErrorId -ne $NotFound){
+            throw $_
+        }
+    }
+    $Found
+}
